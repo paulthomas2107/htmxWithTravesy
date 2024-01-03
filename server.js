1b0ecab332b84aac700718f3f811e9dfd0aef5bd
Paul Thomas <paulthomas2107@gmail.com>
2024-01-03 10:41:34 +0000

    aaa1

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/users', (req, res) => {
+  const users = [
+    {
+      id: 123,
+      name: 'John Doe',
+    },
+    {
+      id: 456,
+      name: 'Rory Thomas',
+    },
+    {
+      id: 789,
+      name: 'Caitlin Thomas',
+    },
+  ];
+
+  res.send(`
+    <h1 class="text-2xl font-bold my-4">Users</h1>
+    <ul>
+    ${users.map((user) => `<li>${user.name}</li>`).join('')}
+    </ul>
+    `);
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
