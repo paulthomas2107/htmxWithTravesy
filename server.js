4179c7e4633805ee5065d036893c1a816906bd20
Paul Thomas <paulthomas2107@gmail.com>
2024-01-03 11:57:39 +0000

    xxxx

--- server.js
+++ server.js
@@ -70,6 +70,39 @@ app.get('/get-temperature', (req, res) => {
   res.send(currentTemp.toFixed(1) + '°C');
 });
 
+app.post('/search/api', async (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+
+  if (!searchTerm) {
+    return res.send('<tr></tr>');
+  }
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const contacts = await response.json();
+
+  const searchResults = contacts.filter((contact) => {
+    const name = contact.name.toLowerCase();
+    const email = contact.email.toLowerCase();
+
+    return name.includes(searchTerm) || email.includes(searchTerm);
+  });
+
+  setTimeout(() => {
+    const searchResultHtml = searchResults
+      .map(
+        (contact) => `
+      <tr>
+        <td><div class="my-4 p-2">${contact.name}</div></td>
+        <td><div class="my-4 p-2">${contact.email}</div></td>
+      </tr>
+    `
+      )
+      .join('');
+
+    res.send(searchResultHtml);
+  }, 1000);
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
