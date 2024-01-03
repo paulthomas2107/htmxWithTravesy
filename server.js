f9bf76c91885391024044cc36a74fcf4d4614b6c
Paul Thomas <paulthomas2107@gmail.com>
2024-01-03 11:07:02 +0000

    more

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
+  /*
   const users = [
     {
       id: 123,
@@ -22,12 +23,18 @@ app.get('/users', (req, res) => {
     },
   ];
 
-  res.send(`
+  */
+
+  setTimeout(async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+    res.send(`
     <h1 class="text-2xl font-bold my-4">Users</h1>
     <ul>
     ${users.map((user) => `<li>${user.name}</li>`).join('')}
     </ul>
     `);
+  }, 2000);
 });
 
 app.listen(3000, () => {
